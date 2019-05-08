20b0a119c2ce54a3874ae72be20ec4045762e892
Jerome Flaherty <jayromulus.f@gmail.com>
2019-05-08 13:05:43 -0400

    day 3 post lunch push

---
+++ 03-loops/01-forLoops.js
@@ -0,0 +1,33 @@
+// FOR LOOPS
+
+/*
+A loop is a rogramming tolol that repeats a set f instructions intil a specified condition is met
+*/
+
+const vacationSpots = ['Florence', 'Copenhagen', 'Istanbul'];
+
+for (i = 0; i < 3; i++) {
+    console.log(vacationSpots[i]);
+}
+
+
+// Types of lops
+/*
+for Loops statement
+do While wtatement
+While statement
+Labeled statemeent
+break statement
+continue statement
+for in statement
+for of statement
+*/
+
+for (i=0; i<10; i++) {
+    console.log(i);
+}
+
+
+for(i=2; i<22; i+=2) {
+    console.log(i);
+}
